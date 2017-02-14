e94a27f3ac58772cc892b378666a1d4b430d5d7c
Robert Canas <robert_canas@hotmail.com>
2017-02-14 20:56:22 +0000

    initializing the tiny-app folder and creation of the express_server.

---
+++ express_server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (require, response) => {
+  response.end("Hello!");
+});
+
+app.get("/urls", (require,response) => {
+  response.render()
+});
+
+app.get("/urls.json", (require, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (require, response) => {
+  response.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
