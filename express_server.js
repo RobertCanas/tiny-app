de5dd5bb7258c02e6b5eae0577a26adf50c8790b
Robert Canas <robert_canas@hotmail.com>
2017-02-15 00:24:33 +0000

    Implementation of submission form, and installation of body-parser npm

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -19,6 +21,15 @@ app.get("/urls", (require,response) => {
   response.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls.json", (require, response) => {
   response.json(urlDatabase);
 });
@@ -28,12 +39,4 @@ app.get("/urls/:id", (require, response) => {
                        url : urlDatabase[require.params.id] };
   console.log(templateVars);
   response.render("urls_show", templateVars);
-});
-
-app.get("/hello", (require, response) => {
-  response.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
