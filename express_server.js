d5fd2c7da7164c17ce0e520b924db7571f1e5faf
Robert Canas <robert_canas@hotmail.com>
2017-02-15 00:48:04 +0000

    Addition of generateRandomString, rearranging views/urls files to add the header on the body

--- express_server.js
+++ express_server.js
@@ -3,7 +3,10 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -11,23 +14,29 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
 app.get("/", (require, response) => {
   response.end("Hello!");
 });
 
-app.get("/urls", (require,response) => {
-  let templateVars = { urls: urlDatabase };
+app.get("/urls", (require, response) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
   // console.log(templateVars);
   response.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/urls/new", (require, response) => {
+  response.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.post("/urls", (require, response) => {
+  console.log(req.body); // debug statement to see POST parameters
+  response.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (require, response) => {
@@ -35,8 +44,13 @@ app.get("/urls.json", (require, response) => {
 });
 
 app.get("/urls/:id", (require, response) => {
-  let templateVars = { shortURL: require.params.id,
-                       url : urlDatabase[require.params.id] };
-  console.log(templateVars);
+  let templateVars = {
+    shortURL: require.params.id,
+    url: urlDatabase[require.params.id]
+  };
   response.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
